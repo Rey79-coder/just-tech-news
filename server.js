8136db9cc10e470f34187bb0d85d5b5adf525c24
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-07-13 19:31:54 -0700

    needs/review

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
+
+
 const app = express();
-const PORT = process.env.PORT || 3067;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,3 +17,12 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
