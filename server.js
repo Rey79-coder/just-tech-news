ba906eef9e54d727fe6f609741aa48ff7c56fa5a
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-07-17 13:24:44 -0700

    nodemon

--- server.js
+++ server.js
@@ -13,5 +13,6 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => 
+  console.log(`Now listening on port ${PORT}!`));
 });
