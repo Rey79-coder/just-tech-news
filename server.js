d440731134535859d839eb6b4a3049a6ae4aa0c9
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-07-19 18:01:12 -0700

    handlebars

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
 const sequelize = require('./config/connection');
 const path = require('path');
 const routes = require('./controllers/');
@@ -7,17 +7,17 @@ const routes = require('./controllers/');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // turn on routes
 app.use(routes);
