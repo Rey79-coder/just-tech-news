e2223be0a5f9153734caddcbab789b9052172cdc
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-07-23 21:41:30 -0700

    feature/dashboard

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const path = require('path');
+const helpers = require('./utils/helpers');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
+// const routes = require('./routes');
+const sequelize = require('./config/connection');
+const path = require('path');
+const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -19,21 +25,23 @@ const sess = {
   })
 };
 
+
 app.use(session(sess));
-
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => 
+  console.log(`Now listening on port ${PORT}!`));
 });
